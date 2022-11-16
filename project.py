959d94e812a22dddf3b7727d426e879dcd38e842
Brian van der Bijl <peikos@peikos.net>
2022-11-16 18:25:16 +0100

    Internationalisatie

--- project.py
+++ project.py
@@ -1,2 +1,9 @@
-def hello_world():
-    print("Hello world!")
+def hello_world(lang):
+    if lang == "NL":
+        print("Hallo wereld!")
+    elif lang == "FR":
+        print("Bonjour le monde!")
+    else:
+        print("Hello world!")
+
+hello_world("EN")
